fea0e376d240461aac192afe709d6f8c0d72e287
Federico Vrigolini <fvirgolini@itr.edu.ar>
2024-11-27 08:42:20 -0300

    esqueleto recuperatorio

---
+++ src/Ejercicio2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Ejercicio2() {
+  return (
+    <div>
+      <h1>Ejercicio modularización</h1>
+    </div>
+  )
+}
---
+++ src/Ejericicio1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Ejericicio1() {
+  return (
+    <div>
+      <h1>Ejercicio formulario</h1>
+    </div>
+  )
+}
